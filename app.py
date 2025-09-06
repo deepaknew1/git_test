c5de44b20409a06aa97b8b9050566cd7167b3a75
DeepakNew <prajapari1210deep@gmail.com>
2025-09-06 14:47:04 +0530

    sample fist app commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import time
+import redis
+from flask import Flask
+
+app= Flask(__name__)
+cache=redis.Redis(host='redis',port=6379)
+
+def get_hit_count():
+    retries=5
+    while True:
+        try:
+            #cache.reset_retry_count()
+            return cache.incr('this')
+        except redis.exceptions.connectionError as exc:
+            if retries==0:
+                raise exc
+            retries=-1
+            time.sleep(1)
+
+@app.route('/')
+def hello():
+    count=get_hit_count()
+    return 'Hello deepak! I have been seen {} times. \n'.format(count)
